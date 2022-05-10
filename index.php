4a7353c682a7caeb06381c7988d5419b60f8e071
Dennis <dennispirotta@gmail.com>
2022-05-10 15:32:54 +0000

    Added my project

---
+++ index.php
@@ -0,0 +1,168 @@
+<?php include("components.php") ?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <?= $GLOBALS["header"] ?>
+    <title>Projects Hub</title>
+</head>
+<body>
+<header>
+    <!-- Intro settings -->
+    <div id="intro" class="p-5 text-center bg-light">
+        <h1 class="mb-3 h2">Progetti Dennis</h1>
+        <p class="mb-3">Archivio dei progetti</p>
+    </div>
+    <!-- Navbar -->
+    <?= $GLOBALS["navbar"] ?>
+    <!-- Navbar -->
+
+
+</header>
+<!--Main Navigation-->
+
+<!--Main layout-->
+<main class="my-5">
+    <div class="container">
+        <!--Section: Content-->
+        <section class="text-center">
+            <h4 class="my-5"><strong>Ultimi progetti</strong></h4>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-12 mb-4">
+                    <div class="card">
+                        <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                            <img src="img/online-payment.svg" class="img-fluid" alt=""/>
+                            <a href="services">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                            </a>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Web Services</h5>
+                            <p class="card-text">
+                                Some quick example text to build on the card title and make up the bulk of the
+                                card's content.
+                            </p>
+                            <a href="" class="btn btn-primary">Vai</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card">
+                        <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                            <img src="img/java-4-logo-svgrepo-com.svg" class="img-fluid" width="300vw" alt=""/>
+                            <a href="java">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                            </a>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Java</h5>
+                            <p class="card-text">
+                                Some quick example text to build on the card title and make up the bulk of the
+                                card's content.
+                            </p>
+                            <a href="php" class="btn btn-primary">Read</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card">
+                        <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                            <img src="img/php.png" class="img-fluid" width="300vw" alt=""/>
+                            <a href="">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                            </a>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Php</h5>
+                            <p class="card-text">
+                                Some quick example text to build on the card title and make up the bulk of the
+                                card's content.
+                            </p>
+                            <a href="" class="btn btn-primary">Read</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--Section: Content-->
+
+        <!-- Pagination -->
+        <nav class="my-4" aria-label="...">
+            <ul class="pagination pagination-circle justify-content-center">
+                <li class="page-item">
+                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item active" aria-current="page">
+                    <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</main>
+<!--Main layout-->
+<!--Footer-->
+<footer class="bg-light text-lg-start">
+    <div class="py-4 text-center">
+        <a role="button" class="btn btn-primary btn-lg m-2"
+           href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA" rel="nofollow" target="_blank">
+            Learn Bootstrap 5
+        </a>
+        <a role="button" class="btn btn-primary btn-lg m-2" href="https://mdbootstrap.com/docs/standard/"
+           target="_blank">
+            Download MDB UI KIT
+        </a>
+    </div>
+
+    <hr class="m-0"/>
+
+    <div class="text-center py-4 align-items-center">
+        <p>Follow MDB on social media</p>
+        <a href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA" class="btn btn-primary m-1" role="button"
+           rel="nofollow" target="_blank">
+            <i class="fab fa-youtube"></i>
+        </a>
+        <a href="https://www.facebook.com/mdbootstrap" class="btn btn-primary m-1" role="button" rel="nofollow"
+           target="_blank">
+            <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="https://twitter.com/MDBootstrap" class="btn btn-primary m-1" role="button" rel="nofollow"
+           target="_blank">
+            <i class="fab fa-twitter"></i>
+        </a>
+        <a href="https://github.com/mdbootstrap/mdb-ui-kit" class="btn btn-primary m-1" role="button" rel="nofollow"
+           target="_blank">
+            <i class="fab fa-github"></i>
+        </a>
+    </div>
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2020 Copyright:
+        <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!--Footer-->
+<!-- MDB -->
+<script type="text/javascript" src="js/mdb.min.js"></script>
+<!-- Custom scripts -->
+<script type="text/javascript" src="js/script.js"></script>
+<style>
+    #intro {
+        margin-top: 58px;
+    }
+    @media (max-width: 991px) {
+        #intro {
+            margin-top: 45px;
+        }
+    }
+</style>
+</body>
+</html>
