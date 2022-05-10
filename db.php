f1eb59c7ce83b1ebbf02d9e4bc935248c5bc3edf
DennisPirotta <63789863+DennisPirotta@users.noreply.github.com>
2022-05-10 23:46:48 +0200

    try to fix db

--- db.php
+++ db.php
@@ -3,13 +3,9 @@ $servername = "localhost";
 $username = "root";
 $password = "v&E%/&YUJKpL:èO)UjnuYF%&G&rkmH7uç;òP:";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 $GLOBALS["db"] = $conn;
-header("Location:  ");
-exit();
