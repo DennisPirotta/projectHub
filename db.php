4613caafd722d4e6505f710f97b27db5dd63a925
DennisPirotta <63789863+DennisPirotta@users.noreply.github.com>
2022-05-11 09:11:43 +0200

    try to fix db

--- db.php
+++ db.php
@@ -2,11 +2,12 @@
 $servername = "localhost";
 $username = "admin";
 $password = 'fD6(:çU)ç_è9jbnBE$YNLç=yuuNTyuln';
+$db = "projecthub";
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password,$db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
-connessione riuscita
+
+$conn->query("SELECT * FROM progetti");
