65842c41ca2de439ddcb72c7c3115f188abb7474
DennisPirotta <63789863+DennisPirotta@users.noreply.github.com>
2022-05-11 09:09:56 +0200

    try to fix db

--- db.php
+++ db.php
@@ -8,9 +8,5 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-echo '<script>console.log("connessione avvenuta correttamente")';
-
-$GLOBALS["db"] = $conn;
-
-print_r($GLOBALS["db"]);
+?>
+connessione riuscita
